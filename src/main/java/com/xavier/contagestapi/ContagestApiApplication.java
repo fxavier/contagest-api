2da2249752bfca0833cdf880eb2f7dfd1309e1cf
Xavier Francisco Nhagumbe <xavierfrancisco353@gmail.com>
2019-07-29 20:27:11 +0200

    Initialize

---
+++ src/main/java/com/xavier/contagestapi/ContagestApiApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.contagestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContagestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContagestApiApplication.class, args);
+	}
+
+}
